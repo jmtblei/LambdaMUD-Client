afa340fc3f48ea7d4c84bdce62275712044e7be9
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-30 09:46:21 -0700

    player directionality

--- mud-client/src/components/Register/Register.js
+++ mud-client/src/components/Register/Register.js
@@ -22,19 +22,6 @@ class Register extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    if (
-      this.state.username === "" ||
-      this.state.password1 === "" ||
-      this.state.password2 === ""
-    ) {
-      console.log("All Fields Required");
-      return;
-    }
-
-    if (this.state.password1 !== this.state.password2) {
-      console.log("Passwords Don't Match");
-    }
-
     const URL = `https://lambda-mud-cs.herokuapp.com/api/registration/`;
     axios
       .post(URL, {
